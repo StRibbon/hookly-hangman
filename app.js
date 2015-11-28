b81148e571a19dc5cb4848ec129d51a20b7bda53
Stephen Ribbon <stribbon@icloud.com>
2015-11-28 14:56:14 -0800

    user added to list on join

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/hookly.js', function(req,res){
   res.sendFile(__dirname + '/node_modules/hookly.js/hookly.min.js');	
 });
+app.get('/jquery', function(req,res){
+  res.sendFile(__dirname + '/node_modules/jquery/dist/jquery.min.js');	
+});
 
 app.get('/shortid.js', function(req,res){
   var b = browserify({
@@ -14,6 +17,10 @@ app.get('/shortid.js', function(req,res){
   });
   b.add('./node_modules/shortid/index.js');
   b.bundle().pipe(res);
+});
+
+app.get('/list', function(req,res){
+  res.sendFile(__dirname + '/public/views/player-list.html');
 });
 
 app.listen(3000, function(){
--- public/app.js
+++ public/app.js
@@ -1,42 +1,67 @@
-var app = angular.module('Hookly-App', ['ui.router', 'ngCookies']);
+var app = angular.module('Hookly-App', ['ngCookies']);
 
-app.config([
-  '$locationProvider',
-  '$stateProvider',
-  '$urlRouterProvider',
-  function($locationProvider, $stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
-    // routes
-    $stateProvider
-      .state('signin', {
-        url: '/signin',
-        templateUrl: 'views/sign-in.html',
-        controller: 'MainCtrl'
-    });
-  }
-]);
-
-app.controller('MainCtrl', function($scope, $cookieStore){
-  $scope.userId = shortid.generate();
-  $scope.loginUser = function(userName){
-    $cookieStore.put('userName', userName);
+app.controller('MainCtrl', function($scope, $cookieStore, $rootScope){
+  
+  $scope.list = [];
+  $scope.user = {};
+  $scope.user.id = shortid.generate();
+  $rootScope.id = $scope.user.id;
+  console.log($rootScope.id);
+  hookly.start('T2rTVLncKjd1G0wuRR8ks22FMeRyzu-UyKfOqmdldXxFIzhV', $scope.user.id);
+  
+  
+  $scope.loginUser = function(name){
+    //$cookieStore.put('name', name);
+    $rootScope.name = name;
+    $scope.user.name = name;
+    hookly.notify('joined', $scope.user);
   };
+
+  hookly.on('joined', function(data){
+      console.log(data);
+      $scope.addUser(data);
+      $scope.getList($scope.user.id);
+      alert(data + " joined");     
+  }); 
+
+  $scope.addUser = function(data) {
+    setTimeout(function() {
+      $scope.list.push(data);
+      console.log('message:' + data);
+      $scope.$apply();
+    }, 0);
+  };
+
+  $scope.getList = function(uid){
+  	hookly.notify('getList', uid);
+  };
+
+  hookly.on('getList', function(uid){
+  	hookly.notify('sendList', uid, $scope.list);
+  });
+
+  hookly.on('sendList', function(data){
+    $scope.list = data;
+    debugger;
+    $scope.$apply();    
+  });
+
 });
 
-
-var userId = shortid.generate();
-console.log(userId);
+//var userId = shortid.generate();
+//console.log(userId);
 
 //window.title = userId;
 
-hookly.start('T2rTVLncKjd1G0wuRR8ks22FMeRyzu-UyKfOqmdldXxFIzhV', userId);
+//hookly.start('T2rTVLncKjd1G0wuRR8ks22FMeRyzu-UyKfOqmdldXxFIzhV', userId);
 
-hookly.notify('message', "Hello Twirl");
+//hookly.notify('message', "Hello Twirl");
 
 //hookly.notify('move', {x, y});
 
-hookly.on('message', function(data){
-  console.log(data);
-});
+// hookly.on('message', function(data){
+//   console.log(data);
+// });
 
 // hookly.notify('message', 'NkbMp16Xx', 'Sup Mane');
+
